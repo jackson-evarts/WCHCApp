b6032aed3f74b2aa0b7742a4212c221284af9874
jackson-evarts <jack.evarts@gmail.com>
2024-10-09 21:02:57 -0400

    Initial Commit

---
+++ WCHCApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WCHCApp
+//
+//  Created by Jackson Evarts on 10/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WCHCApp/WCHCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WCHCAppApp.swift
+//  WCHCApp
+//
+//  Created by Jackson Evarts on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WCHCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
