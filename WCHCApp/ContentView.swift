66835724cd78f95bebd15362100e6efabbb220ec
jackson-evarts <jack.evarts@gmail.com>
2024-10-09 21:45:24 -0400

    Initial view and transition done

--- WCHCApp/ContentView.swift
+++ WCHCApp/ContentView.swift
@@ -9,42 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationOffset: CGFloat = -500 // Start stripes above the screen
+    @State private var animationOffset: CGFloat = -1000 // Start stripes above the screen
     
     var body: some View {
-        ZStack {
-            // Background stripes
-            Color.darkPurple.ignoresSafeArea()
-            
-            // Stripes animation handling:
-            GeometryReader { geometry in
-                ZStack {
-                    ForEach(0..<6) { index in // Adjust the number of stripes
-                        Color.lightRed
-                            .frame(width: geometry.size.width * 2, height: 60) // Width longer than the screen to allow for diagonal animation
-                            .rotationEffect(.degrees(30)) // Rotate the stripe by 30 degrees
-                            .offset(x: -150, y: animationOffset + CGFloat(index * 150)) // Stagger the starting positions of stripes
+        NavigationStack{
+            ZStack {
+                // Background stripes
+                Color.darkPurple.ignoresSafeArea()
+                
+                // Stripes animation handling:
+                GeometryReader { geometry in
+                    ZStack {
+                        ForEach(0..<6) { index in // Adjust the number of stripes
+                            Color.lightRed
+                                .frame(width: geometry.size.width * 2, height: 60) // Width longer than the screen to allow for diagonal animation
+                                .rotationEffect(.degrees(30)) // Rotate the stripe by 30 degrees
+                                .offset(x: -150, y: animationOffset + CGFloat(index * 150)) // Stagger the starting positions of stripes
+                        }
+                    }
+                    .onAppear {
+                        withAnimation(
+                            Animation.linear(duration: 20)
+                                .repeatForever(autoreverses: false)
+                        ) {
+                            // Loop the animation moving stripes from the top to the bottom
+                            animationOffset = geometry.size.height + 1000
+                        }
                     }
                 }
-                .onAppear {
-                    withAnimation(
-                        Animation.linear(duration: 20)
-                            .repeatForever(autoreverses: false)
-                    ) {
-                        // Loop the animation moving stripes from the top to the bottom
-                        animationOffset = geometry.size.height + 800
+                
+                VStack {
+                    
+                    NavigationLink(destination: RadioPlayerView()){
+                        Text("Play Radio")
+                            .font(.title)
+                            .foregroundColor(.beige)
+                            .padding()
                     }
+                    
                 }
+                .padding()
             }
-            
-            
-            VStack {
-                Text("WCHC App")
-                    .font(.title)
-                    .foregroundColor(.beige)
-                    .padding()
-            }
-            .padding()
         }
     }
 }
---
+++ WCHCApp/RadioPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  RadioPlayerView.swift
+//  WCHCApp
+//
+//  Created by Jackson Evarts on 10/9/24.
+//
+
+import SwiftUI
+
+struct RadioPlayerView: View {
+    var body: some View {
+        Text("I am playing the radio")
+    }
+}
+
+#Preview {
+    RadioPlayerView()
+}
