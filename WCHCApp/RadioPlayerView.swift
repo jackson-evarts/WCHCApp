1c05402bbd44cc96118d9c2aea00c5d2367fa7b5
jackson-evarts <jack.evarts@gmail.com>
2024-10-13 13:34:04 -0400

    Full radio implementation, working and functioning. Just need to build out the UI to look really cool and then it's basically finished lol.

--- WCHCApp/RadioPlayerView.swift
+++ WCHCApp/RadioPlayerView.swift
@@ -6,10 +6,55 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct RadioPlayerView: View {
+    @State private var player: AVPlayer?
+    @State private var isPlaying = false
+
     var body: some View {
-        Text("I am playing the radio")
+        ZStack{
+            
+            // Background color
+            Color.darkPurple.ignoresSafeArea()
+
+            VStack {
+                Button(action: {
+                    if self.isPlaying {
+                        self.player?.pause()
+                    } else {
+                        self.playRadio()
+                    }
+                    self.isPlaying.toggle()
+                }) {
+                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(Color.beige)
+
+                }
+                .padding()
+                
+                Text(isPlaying ? "Now Playing" : "Paused")
+                    .font(.title)
+                    .padding()
+                    .foregroundStyle(Color.beige)
+
+            } // Close VStack
+            .onAppear {
+                // Setup AVPlayer when the view appears
+                let url = URL(string: "https://s2.radio.co/sc161fe4c9/listen")!
+                self.player = AVPlayer(url: url)
+            }
+            
+            
+        }
+    }
+
+    
+    // Functions:
+    func playRadio() {
+        self.player?.play()
     }
 }
 
